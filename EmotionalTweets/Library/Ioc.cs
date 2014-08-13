ac0c3658d50ec8d7e679ee39a4250e520e98bc49
Craig Godden-Payne <craig@goddenpayne.co.uk>
2014-08-13 22:59:27 +0100

    Added ioc framework and some configuration

---
+++ EmotionalTweets/Library/Ioc.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Autofac;
+using EmotionalTweets.Helpers;
+using EmotionalTweets.RequestFactory;
+using EmotionalTweets.ServiceAdapters;
+
+namespace EmotionalTweets
+{
+    public class Ioc
+    {
+        public static IContainer Container { get; private set; }
+
+        public static void Register()
+        {
+            var container = new ContainerBuilder();
+            //container.Register<IObjectSerializer>(x => new ObjectSerializer());
+            
+            container.Register<ITwitterApiAdapter>(x => new TwitterApiAdapter(
+                x.Resolve<ITwitterApiRequestFactory>(),
+                x.Resolve<IHttpHelper>(),
+                x.Resolve<IObjectSerializer>()));
+
+            container.Register<ITwitterApiRequestFactory>(x => new TwitterApiRequestFactory());
+            container.Register<IHttpHelper>(x => new HttpHelper());
+            Container = container.Build();
+        }
+
+        public static TResult Resolve<TResult>()
+        {
+            return Container.Resolve<TResult>();
+        }
+    }
+
+}
