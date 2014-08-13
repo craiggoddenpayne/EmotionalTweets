1bc74fbdc83029bbc0448ac90a673e93fc6fbd6c
Craig Godden-Payne <craig@goddenpayne.co.uk>
2014-08-13 21:38:42 +0100

    Created Solution Structure

---
+++ EmotionalTweets/Android/MainActivity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+using Xamarin.Forms.Platform.Android;
+
+
+namespace EmotionalTweets.Android
+{
+	[Activity (Label = "EmotionalTweets.Android.Android", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	public class MainActivity : AndroidActivity
+	{
+	}
+}
+
