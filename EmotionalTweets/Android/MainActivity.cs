d34d013b304f05f7927b4dcd1bd75e664faf46a8
Craig Godden-Payne <craig@goddenpayne.co.uk>
2014-08-14 21:21:40 +0100

    Created submit button handler

--- EmotionalTweets/Android/MainActivity.cs
+++ EmotionalTweets/Android/MainActivity.cs
@@ -1,7 +1,10 @@
-﻿using Android.App;
+﻿using System;
+using Android.App;
+using Android.Content;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using EmotionalTweets;
 
 namespace EmotionalTweetsAndroid
 {
@@ -11,19 +14,34 @@ namespace EmotionalTweetsAndroid
 	    private Button _submit;
 	    private ProgressBar _progress;
 	    private EditText _searchField;
+	    private IEmotionalTweetsApplication _application;
 
 	    protected override void OnCreate(Bundle bundle)
 		{
 			RequestWindowFeature(WindowFeatures.NoTitle);
 			base.OnCreate(bundle);
 		    SetContentView(Resource.Layout.EmotionalTweetsSearch);
+            _application = Ioc.Resolve<IEmotionalTweetsApplication>();
 
             _progress = (ProgressBar)FindViewById(Resource.Id.Progress);
 			_searchField = (EditText)FindViewById(Resource.Id.SearchField);
 			_submit = (Button)FindViewById(Resource.Id.Submit);
 
 			_progress.Visibility = ViewStates.Invisible; 
+            _submit.Click += HandleClick;
 		}
+
+	    private async void HandleClick(object sender, EventArgs e)
+	    {
+	        _progress.Visibility = ViewStates.Visible;
+	        _submit.Enabled = false;
+
+	        RunOnUiThread(() =>
+	        {
+	            _progress.Visibility = ViewStates.Invisible;
+	            _submit.Enabled = true;
+	        });
+	    }
 	}
 }
 
