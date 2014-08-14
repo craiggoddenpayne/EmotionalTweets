6ad84fd69fddfdd1b3fe388e31a168dbe6df67fe
Craig Godden-Payne <craig@goddenpayne.co.uk>
2014-08-14 20:29:24 +0100

    Created Ioc entry point

---
+++ EmotionalTweets/Android/EmotionalTweetsApplication.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Android.App;
+using Android.Runtime;
+
+namespace EmotionalTweets.Android
+{
+    [Application(Debuggable = true, Label = "Emotional Tweets")]
+    public class EmotionalTweetsApplication : Application
+    {
+        public EmotionalTweetsApplication(IntPtr ptr, JniHandleOwnership ownership)
+            : base(ptr, ownership)
+        {
+        }
+
+        public override void OnCreate()
+        {
+            base.OnCreate();
+            Ioc.Register();
+        }
+    }
+}
--- EmotionalTweets/Android/MainActivity.cs
+++ EmotionalTweets/Android/MainActivity.cs
@@ -1,19 +1,11 @@
-﻿using System;
-
-using Android.App;
-using Android.Content;
+﻿using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
-using Android.OS;
-
 using Xamarin.Forms.Platform.Android;
 
 
 namespace EmotionalTweets.Android
 {
-	[Activity (Label = "EmotionalTweets.Android.Android", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+	[Activity (Label = "Emotional Tweets", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
 	public class MainActivity : AndroidActivity
 	{
 	}
